b318c9652c5417f5332a54bfb6d8b42e61a23ffe
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-26 12:04:41 +0300

    scheme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: 'color-scheme', content: 'dark' },
+      ],
+      htmlAttrs: {
+        class: 'dark',
+      },
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
