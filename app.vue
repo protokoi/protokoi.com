15238296a5c1f338d7268feed05373106a81e681
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-26 11:47:42 +0300

    sign

--- app.vue
+++ app.vue
@@ -1,27 +1,22 @@
 <template>
-  <div class="bg-neutral-950 text-white w-screen h-screen flex flex-col gap-2 justify-center items-center">
-    <label class="doto"> {{ "protokoi.com" }} </label>
-    <label class="doto"> {{ "coming_soon" }} </label>
+  <div class="bg-neutral-900 text-white doto font-extralight fixed w-screen h-screen flex flex-col gap-2 justify-center items-center">
+    <label class=""> {{ "protokoi.com" }} </label>
+    <label class=""> {{ "coming_soon" }} </label>
+
+    <a target="_blank" href="https://github.com/protokoi" class="absolute bottom-0 flex items-center gap-1 m-2 duration-300 hover:text-green-400 hover:m-3">
+      <p class=""> {{ "protokoi" }} </p>
+      <Icon name="pixelarticons:github" class="text-xl opacity-70" />
+    </a>
   </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Doto&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Yrsa&display=swap');
-
 .doto {
   font-family: "Doto", serif;
   font-optical-sizing: auto;
-  font-weight: 400;
   font-style: normal;
   font-variation-settings:
-    "ROND" 0;
-}
-
-.yrsa {
-  font-family: "Yrsa", serif;
-  font-optical-sizing: auto;
-  font-weight: 400;
-  font-style: normal;
+    "ROND" ;
 }
 </style>
